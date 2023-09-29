dfed68e4a81c817862a7032e4d18bc4beeeea530
SarahGuarneri <sarahguarneri97@icloud.com>
2023-09-29 16:49:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// const header = document.querySelector('header')
+// const nav = document.querySelector('nav')
+
+
+// window.addEventListener('scroll', function(){
+//     const halfHeaderHeigth = header.clientHeight / 2
+//     if (this.window.scrollY > halfHeaderHeigth) {
+//         nav.classList.add('animated')
+//     } else {
+//         nav.classList.remove('animated')
+//     }
+// })
